3a9ab93cfa9777daa6595f4ad44a8f49c0270da9
LuciaCavana <luciacavana@yahoo.com.ar>
2023-06-26 22:15:13 -0300

    excepción

---
+++ src/excepciones/AtSymbolNotFoundException.java
@@ -0,0 +1,14 @@
+package excepciones;
+
+public class AtSymbolNotFoundException extends Exception {
+
+	public AtSymbolNotFoundException(String mail) {
+        super(mail);
+    }
+	
+	public void validateAtSymbol(String text) throws AtSymbolNotFoundException {
+        if (!text.contains("@")) {
+            throw new AtSymbolNotFoundException("El texto no contiene el símbolo '@': " + text);
+        }
+    }
+}
